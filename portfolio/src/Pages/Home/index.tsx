bde04f0f76ac00bc465844d845553f3b06fcd2aa
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-01-21 18:55:12 -0300

    feat: estrutura feita

--- portfolio/src/Pages/Home/index.tsx
+++ portfolio/src/Pages/Home/index.tsx
@@ -1,9 +1,5 @@
 import { Header } from "../../Components/Header";
 
 export const Home = () => {
-<<<<<<< HEAD
   return;
-=======
-  return <Header />;
->>>>>>> 7835840d684fe6f8fff06fea2aa3c55cad9aae07
 };
