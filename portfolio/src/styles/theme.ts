af1e02df9f38a5efae47c2a223b211e063da0015
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-01-23 11:31:37 -0300

    feat: temas adicionados

--- portfolio/src/styles/theme.ts
+++ portfolio/src/styles/theme.ts
@@ -1,30 +1,47 @@
 export const theme = {
   color: {
-    primary1: {
-      main: "",
+    first: {
+      main: "#023047",
     },
-    secundary: {
-      main: "",
+    second: {
+      main: "#FB8500",
+    },
+    third: {
+      main: "#FFB703",
+    },
+    fourth: {
+      main: "#219EBC",
+    },
+    fifth: {
+      main: "#8ECAE6",
     },
     grey: {
-      one: "",
-    },
-    toast: {
-      negative: "",
-      warning: "",
-      success: "",
-      information: "",
+      one: "#212529",
+      two: "#495057",
+      three: "#adb5bd",
+      four: "#dee2e6",
+      five: "#f1f3f5",
     },
   },
 
   border: {
     radius: {
+      one: "12px",
+      two: "8px",
+      three: "4px",
+    },
+    shadows: {
       one: "",
     },
   },
 
   font: {
-    one: "",
+    title1: "32px 700 Georgia, serif",
+    title2: "28px 700 Georgia, serif",
+    title3: "24px 700 Georgia, serif",
+    text1: "18px 700 Georgia, serif",
+    text2: "16px 700 Georgia, serif",
+    text3: "12px 700 Georgia, serif",
   },
 };
 
