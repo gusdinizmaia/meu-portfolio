1592fe74c506fa6015dfe62d8a4e16e1ec2a0f09
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-01-23 18:58:20 -0300

    fix: retirado components da page

--- portfolio/src/Pages/Home/index.tsx
+++ portfolio/src/Pages/Home/index.tsx
@@ -1,12 +1,5 @@
-import { BestProjects } from "../../Components/BestProjects";
-import { Projects } from "../../Components/Projects";
 import { HomeStyle } from "./style";
 
 export const Home = () => {
-  return (
-    <HomeStyle>
-      <Projects />
-      <BestProjects />
-    </HomeStyle>
-  );
+  return <HomeStyle></HomeStyle>;
 };
